499979081254a8d425602e2c73f68af3cda09475
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-08-25 23:51:19 +0530

    Frontend Routes (BrowserRouter, Routes, Route)

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "screens/homePage";
+import LoginPage from "screens/loginPage";
+import ProfilePage from "screens/profilePage";
+import Navbar from "screens/navbar";
+
 
 function App() {
+  
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element= {<LoginPage />} />
+          <Route path="/home" element= {<HomePage />} />
+          <Route path="/profile/:userId" element= {<ProfilePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
