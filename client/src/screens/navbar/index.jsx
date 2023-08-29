ea1c965b84839fc38c4952401b96afc5bfa72c4f
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-08-29 23:04:56 +0530

    Register, Login Page

--- client/src/screens/navbar/index.jsx
+++ client/src/screens/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          SoCiAlMeDiA
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
