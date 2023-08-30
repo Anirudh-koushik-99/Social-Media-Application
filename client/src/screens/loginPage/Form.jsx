3b1d4cf79a2a91b1919ce25f1391aaaf38c3ba1b
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-08-30 20:38:21 +0530

    Login Page logic and functionalities

--- client/src/screens/loginPage/Form.jsx
+++ client/src/screens/loginPage/Form.jsx
@@ -7,14 +7,16 @@ import {
   Typography,
   useTheme,
 } from "@mui/material";
-import { EditOutlinedIcon } from "@mui/icons-material/EditOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined"
 import { Formik } from "formik";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setLogin } from "state";
 import Dropzone from "react-dropzone";
 import FlexBetween from "components/FlexBetween";
+
+
 
 const registerSchema = yup.object().shape({
   firstName: yup.string().required("required"),
@@ -55,7 +57,51 @@ const Form = () => {
   const isLogin = pageType === "login";
   const isRegister = pageType === "register";
 
-  const handleFormSubmit = async (values, onSubmitProps) => {};
+  const register = async(values, onSubmitProps) => {
+    //THIS ALLOWS US TO SEND FORM INFO WITH IMAGE
+    const formData = new FormData()
+    for(let value in values){
+      formData.append(value, values[value])
+    }
+    formData.append('picturePath', values.picture.name)
+    const savedUserResponse = await fetch("http://localhost:3001/auth/register",
+    {
+      method: "POST",
+      body: formData,
+    }
+    );
+    const savedUser = await savedUserResponse.json()
+    onSubmitProps.resetForm();
+  
+    if(savedUser){
+      setPageType("login");
+    }
+  }
+  
+  const login = async(values, onSubmitProps) => {
+    const loggedInResponse = await fetch("http://localhost:3001/auth/login",
+    {
+      method: "POST",
+      headers: {"Content-Type" : "application/json"},
+      body: JSON.stringify(values),
+    }
+    );
+    const loggedIn = await loggedInResponse.json()
+    onSubmitProps.resetForm();
+    if(loggedIn){
+      dispatch(setLogin({
+        user: loggedIn.user,
+        token: loggedIn.token
+      }))
+      navigate("/home")
+    }
+  }
+
+  const handleFormSubmit = async (values, onSubmitProps) => {
+    if(isLogin) await login(values, onSubmitProps);
+    if(isRegister) await registerSchema(values, onSubmitProps)
+
+  };
 
   return (
     <Formik
