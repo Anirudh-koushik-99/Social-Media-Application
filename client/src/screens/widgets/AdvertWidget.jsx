170d6289eaf809d06bc9d37757a01a23e0ec3457
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-09-08 23:50:24 +0530

    Profile Page

--- client/src/screens/widgets/AdvertWidget.jsx
+++ client/src/screens/widgets/AdvertWidget.jsx
@@ -3,35 +3,35 @@ import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
 
 const AdvertWidget = () => {
-    const {palette} = useTheme();
-    const dark = palette.neutral.dark;
-    const main = palette.neutral.main;
-    const medium = palette.neutral.medium; 
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
 
-    return (
-        <WidgetWrapper>
-            <FlexBetween>
-                <Typography color={dark} variant="h5" fontWeight="500">
-                    Sponsored
-                </Typography>
-                <Typography color={medium}>
-                    Create Ad
-                </Typography>
-            </FlexBetween>
-            <img src="http://localhost:3001/assets/info4.jpeg" alt="advert" width="100%" height="auto" style={{borderRadius: "0.75rem", margin: "0.75rem 0"}}/>
-            <FlexBetween>
-                <Typography color={main}>
-                    Advert 1 !!!!
-                </Typography>
-                <Typography color={medium}>
-                    Advert1.com
-                </Typography>
-                <Typography color={medium} m="0.5rem 0">
-                    Advert1 Description Here
-                </Typography>
-            </FlexBetween>
-        </WidgetWrapper>
-    )
-}
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+      <img
+        src="http://localhost:3001/assets/info4.jpeg"
+        alt="advert"
+        width="100%"
+        height="auto"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>Advert 1 !!!!</Typography>
+        <Typography color={medium}>Advert1.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Advert1 Description Here
+      </Typography>
+    </WidgetWrapper>
+  );
+};
 
 export default AdvertWidget;
